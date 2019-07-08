6cdf9c4b510872e2f5a6cb023be9ff13499dfd59
monologg <adieujw@gmail.com>
2019-07-09 00:18:17 +0900

    add new local_test shell file

---
+++ local_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+EPOCH_NUM=$1
+OUTPUT_FILE="predictions_epoch_$EPOCH_NUM.json"
+python3 predict_server.py <(cat mrqa-dev-data/*.jsonl) $OUTPUT_FILE 8888
+for data_file in `ls mrqa-dev-data/*.jsonl`; do base=$(echo ${data_file} | cut -d "/" -f2);python3 mrqa_official_eval.py ${data_file} $OUTPUT_FILE > eval_${base::-1}; done
